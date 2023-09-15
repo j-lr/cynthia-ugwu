3ab2e077e07e4b71dd319cf3d80b71bacabe1f14
OS X <rishabhdhawan@icloud.com>
2023-09-15 19:43:57 +0530

    implementing hover feature along with move with mouse on page2

--- index.js
+++ index.js
@@ -18,8 +18,107 @@ async function loadPages() {
     }
     try {
         await loadPage("pages/page2.html", "div", "page2");
+        onPage1Loaded();
     } catch (e) {
         console.log(e);
+    }
+}
+
+function onPage1Loaded() {
+    const row0 = document.getElementById("row0");
+    const plugImg = document.getElementById("plugImg");
+    const row1 = document.getElementById("row1");
+    const ixperienceImg = document.getElementById("ixperienceImg");
+    const row2 = document.getElementById("row2");
+    const huduImg = document.getElementById("huduImg");
+
+    if (
+        row0 == null ||
+        row1 == null ||
+        row2 == null ||
+        plugImg == null ||
+        ixperienceImg == null ||
+        huduImg == null
+    ) {
+        console.error(
+            "All elements required for Page1 animations not found in DOM "
+        );
+        return;
+    }
+
+    if (row0 && plugImg) {
+        function onPlugHoverEnter() {
+            plugImg.style.display = "block";
+            ixperienceImg.style.display = "none";
+            huduImg.style.display = "none";
+        }
+
+        function onPlugHoverLeave() {
+            plugImg.style.display = "none";
+        }
+        function onIxperienceHoverEnter() {
+            plugImg.style.display = "none";
+            ixperienceImg.style.display = "block";
+            huduImg.style.display = "none";
+        }
+
+        function onIxperienceHoverLeave() {
+            ixperienceImg.style.display = "none";
+        }
+        function onHuduHoverEnter() {
+            plugImg.style.display = "none";
+            ixperienceImg.style.display = "none";
+            huduImg.style.display = "block";
+        }
+
+        function onHuduHoverLeave() {
+            huduImg.style.display = "none";
+        }
+
+        window.addEventListener("mousemove", (e) => {
+            const row0Rect = row0.getBoundingClientRect();
+            const plugRect = plugImg.getBoundingClientRect();
+            const row1Rect = row1.getBoundingClientRect();
+            const ixperienceRect = ixperienceImg.getBoundingClientRect();
+            const row2Rect = row2.getBoundingClientRect();
+            const huduRect = huduImg.getBoundingClientRect();
+            if (
+                e.clientX >= row0Rect.left &&
+                e.clientX <= row0Rect.right &&
+                e.clientY >= row0Rect.top &&
+                e.clientY <= row0Rect.bottom
+            ) {
+                onPlugHoverEnter();
+                const transY = e.clientY - row0Rect.top - plugRect.height;
+                plugImg.style.transform = `translate(${e.clientX}px,${transY}px)`;
+            } else {
+                onPlugHoverLeave();
+            }
+            if (
+                e.clientX >= row1Rect.left &&
+                e.clientX <= row1Rect.right &&
+                e.clientY >= row1Rect.top &&
+                e.clientY <= row1Rect.bottom
+            ) {
+                onIxperienceHoverEnter();
+                const transY = e.clientY - row1Rect.top - ixperienceRect.height;
+                ixperienceImg.style.transform = `translate(${e.clientX}px,${transY}px)`;
+            } else {
+                onIxperienceHoverLeave();
+            }
+            if (
+                e.clientX >= row2Rect.left &&
+                e.clientX <= row2Rect.right &&
+                e.clientY >= row2Rect.top &&
+                e.clientY <= row2Rect.bottom
+            ) {
+                onHuduHoverEnter();
+                const transY = e.clientY - row2Rect.top - huduRect.height;
+                huduImg.style.transform = `translate(${e.clientX}px,${transY}px)`;
+            } else {
+                onHuduHoverLeave();
+            }
+        });
     }
 }
 
@@ -54,8 +153,9 @@ async function fetchHtmlPage(page) {
 function onLanderPageLoaded() {
     window.addEventListener("mousemove", function (e) {
         if (mouseCircle) {
-            mouseCircle.style.transform = `translate(${e.clientX}px, 
-            ${e.clientY}px)`;
+            const posX = e.clientX + window.scrollX;
+            const posY = e.clientY + window.scrollY;
+            mouseCircle.style.transform = `translate(${posX}px, ${posY}px)`;
             mouseCircle.style.display = "block";
         }
     });
