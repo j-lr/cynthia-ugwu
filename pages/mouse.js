d6b1e43fb2335140c4e9b00716c79a7abc96e149
OS X <rishabhdhawan@icloud.com>
2023-09-19 10:07:46 +0530

    code improvements

--- pages/mouse.js
+++ pages/mouse.js
@@ -1,16 +1,15 @@
 "use strict";
 
 import { clamp } from "../utils.js";
-let prevX = 0,
-  prevY = 0,
-  deltaX = 0,
-  deltaY = 0;
+let prevMoveX = 0,
+  prevMoveY = 0,
+  scaleX = 0;
 
 const minScaleX = 1.09;
 const maxScaleX = 1.17;
 let angle = 0;
 const maxAngle = 180;
-const winWidth = window.innerWidth / 1;
+
 const minDelta = 8;
 let throttleX = 0;
 let throttleY = 0;
@@ -29,32 +28,30 @@ window.addEventListener("mousemove", function (e) {
     insideThrottle = true;
   } else insideThrottle = false;
 
-  console.log(insideThrottle);
   prevThrottleX = e.clientX;
   prevThrottleY = e.clientY;
-  deltaX = winWidth / (posX - prevX);
-  angle = clamp(deltaX / 20, -maxAngle, maxAngle);
-  angle = calculateAngle(posX, posY, posX - prevX, posY - prevY);
+  scaleX = window.innerWidth / (posX - prevMoveX);
+  angle = clamp(scaleX / 20, -maxAngle, maxAngle);
+  angle = calculateAngle(posX, posY, posX - prevMoveX, posY - prevMoveY);
   angle = clamp(angle, -90, maxAngle);
-  deltaX = clamp(deltaX, minScaleX, maxScaleX);
-  deltaY = posY - prevY;
+  scaleX = clamp(scaleX, minScaleX, maxScaleX);
 
   mouseCircle.style.display = "block";
-  prevX = posX;
-  prevY = posY;
+  prevMoveX = posX;
+  prevMoveY = posY;
 
   gsap.to(mouseCircle, {
     translateX: posX,
     y: posY,
     duration: 0.46,
-    scaleX: insideThrottle ? 1 : deltaX,
-    scaleY: insideThrottle ? 1 : deltaX - 1,
+    scaleX: insideThrottle ? 1 : scaleX,
+    scaleY: insideThrottle ? 1 : scaleX - 1,
     rotateY: insideThrottle ? 0 : angle,
   });
 
   setTimeout(() => {
-    prevX = posX;
-    prevY = posY;
+    prevMoveX = posX;
+    prevMoveY = posY;
 
     gsap.to(mouseCircle, {
       translateX: posX,
@@ -69,9 +66,9 @@ window.addEventListener("mousemove", function (e) {
 
 function calculateAngle(x1, y1, x2, y2) {
   // Calculate the angle in radians
-  const deltaX = x2 - x1;
+  const scaleX = x2 - x1;
   const deltaY = y2 - y1;
-  const radians = Math.atan2(deltaY, deltaX);
+  const radians = Math.atan2(deltaY, scaleX);
 
   // Convert radians to degrees
   const degrees = radians * (180 / Math.PI);
